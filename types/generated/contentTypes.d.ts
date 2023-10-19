bf1893682c19076667bc135ff55650c9b86c7143
Code with Bahri <codewithbahri@gmail.com>
2023-10-19 09:31:38 +0700

    api order

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -713,6 +713,43 @@ export interface ApiCategoryCategory extends Schema.CollectionType {
   };
 }
 
+export interface ApiOrderOrder extends Schema.CollectionType {
+  collectionName: 'orders';
+  info: {
+    singularName: 'order';
+    pluralName: 'orders';
+    displayName: 'order';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    items: Attribute.JSON;
+    totalPrice: Attribute.Decimal;
+    deliveryAddress: Attribute.String;
+    courierName: Attribute.String;
+    courierPrice: Attribute.Integer;
+    status: Attribute.Enumeration<
+      ['waiting-payment', 'packaging', 'on-delivery', 'done', 'cancel']
+    >;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface ApiProductProduct extends Schema.CollectionType {
   collectionName: 'products';
   info: {
@@ -770,6 +807,7 @@ declare module '@strapi/types' {
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
       'api::category.category': ApiCategoryCategory;
+      'api::order.order': ApiOrderOrder;
       'api::product.product': ApiProductProduct;
     }
   }
